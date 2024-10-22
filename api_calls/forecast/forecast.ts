20283bc856a7d46a6b83068097b90c28c937b425
Lamech Israel <lamechslearning@gmail.com>
2024-10-22 18:22:01 -0500

    Allow call to Weather API

--- api_calls/forecast/forecast.ts
+++ api_calls/forecast/forecast.ts
@@ -1,20 +1,29 @@
-export default async function forecast(zipcode: string, numberOfDays?: string) {
+export default async function fetchForecast(
+  zipcode: number,
+  numberOfDays?: number
+) {
   let response;
 
   if (typeof numberOfDays === "undefined") {
     response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?q${zipcode}`,
+      `http://api.weatherapi.com/v1/forecast.json?q=${zipcode}`,
       {
         method: "GET",
-        headers: { Authorization: `Bearer ${process.env.key}` },
+        headers: {
+          key: `${process.env.key as string}`,
+        },
       }
     );
   } else {
+    console.log("Get forecast");
+    numberOfDays = 1;
     response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?q${zipcode}&days=${numberOfDays}`,
+      `http://api.weatherapi.com/v1/forecast.json?q=${zipcode}&days=${numberOfDays}`,
       {
         method: "GET",
-        headers: { Authorization: `Bearer ${process.env.key}` },
+        headers: {
+          key: `${process.env.key}`,
+        },
       }
     );
   }
