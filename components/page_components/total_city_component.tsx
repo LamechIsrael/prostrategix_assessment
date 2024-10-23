50fef1f59355fb7e201546825a60942dbe13a693
Lamech Israel <lamechslearning@gmail.com>
2024-10-22 20:37:43 -0500

    Add other data.

--- components/page_components/total_city_component.tsx
+++ components/page_components/total_city_component.tsx
@@ -1,11 +1,17 @@
 import FiveDayForcast from "./5-day-forecast";
 import CityInfo from "./city_info";
 
-export default function TotalCityComponent(
-  temperature: number,
-  forecast: Forecast,
-  unit: "F" | "C"
-) {
+interface CityData {
+  temperature: number;
+  forecast: Forecast;
+  unit: "F" | "C";
+}
+
+export default function TotalCityComponent({
+  temperature,
+  forecast,
+  unit,
+}: CityData) {
   return (
     <div>
       <CityInfo
