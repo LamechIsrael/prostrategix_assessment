2aab3dd6b1bd3f610e963b63b5f019175db3c3b7
Lamech Israel <lamechslearning@gmail.com>
2024-10-22 21:33:34 -0500

    Get data to appear on main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useEffect, useState } from "react";
 import getForcast from "../../components/getForecast";
 import CityForm from "../../components/page_components/city_form";
-import { FormEventHandler } from "react";
+import TotalCityComponent from "../../components/page_components/total_city_component";
+import { emptyForecastProjection } from "../../components/empty_variables/forecastProjection";
 
-export default async function Home() {
-  const data = await getForcast(46407, 3);
+export default function Home() {
+  const [zipcode, setZipCode] = useState<number>();
+  const [forecastData, setForecast] = useState<Forecast>();
+  const [tempUnit, setUnit] = useState<string>();
 
-  function submitForm(event: React.FormEvent) {}
+  useEffect(() => {
+    if (zipcode) {
+      const forecast = getForcast(zipcode, 5).then((data) =>
+        setForecast(data as unknown as Forecast)
+      );
+    }
+  });
+
+  function submitForm(event: React.FormEvent) {
+    event.preventDefault();
+    // Get the data that is in the form
+    const fd = new FormData(event.target as any);
+    // Turn the form data into an object of named key-value pairs
+    const data = Object.fromEntries(fd.entries());
+    const zipcode = parseInt(data["zipcode"] as string);
+    const unit = data["temp"] as string;
+
+    setUnit(unit);
+    setZipCode(zipcode);
+  }
 
   return (
     <>
       <CityForm submitFormHandler={submitForm} />
+      {forecastData ? (
+        <TotalCityComponent
+          temperature={
+            tempUnit === "F"
+              ? forecastData?.current.temp_f
+              : forecastData.current.temp_c
+          }
+          forecast={forecastData}
+          unit={tempUnit as "F" | "C"}
+        />
+      ) : (
+        <TotalCityComponent
+          temperature={0}
+          forecast={emptyForecastProjection}
+          unit={"F"}
+        />
+      )}
     </>
   );
 }
