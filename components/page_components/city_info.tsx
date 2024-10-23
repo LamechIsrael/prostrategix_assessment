635f0468b0c83da9202e255e8b1c419aa08a3768
Lamech Israel <lamechslearning@gmail.com>
2024-10-22 20:08:01 -0500

    Add section for displaying city-data.

--- components/page_components/city_info.tsx
+++ components/page_components/city_info.tsx
@@ -1,1 +1,26 @@
-export default function 
+interface CityData {
+  temperature: number;
+  unit: "F" | "C";
+  location: string;
+  icon: string;
+}
+
+export default function CityInfo({
+  temperature,
+  unit,
+  location,
+  icon,
+}: CityData) {
+  return (
+    <div>
+      <img
+        src={icon}
+        alt="An image displaying whether the sky is cloudy, sunny, or rainy"
+      />
+      <h1>
+        {temperature} {unit}
+      </h1>
+      <h1>{location}</h1>
+    </div>
+  );
+}
